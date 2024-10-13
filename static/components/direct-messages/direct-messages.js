37c02d80a117da1336a2cb57fb9aaaffdf360563
Patrick Mulligan <patjmulligan@gmail.com>
2024-10-13 13:04:48 +0200

    don't know if this did anything but shifted fct calls around

--- static/components/direct-messages/direct-messages.js
+++ static/components/direct-messages/direct-messages.js
@@ -167,12 +167,12 @@ async function directMessages(path) {
       }
     },
     created: async function () {
-      await this.getCustomers()
-      this.getCustomersDebounced = _.debounce(this.getCustomers, 2000, false)
       this.newPublicKey="ffcb87baede7afc5f43aa5b27453b4b2168dfb96ebfe963ce5704426e97817f1"
       await this.addPublicKey()
       this.activePublicKey="ffcb87baede7afc5f43aa5b27453b4b2168dfb96ebfe963ce5704426e97817f1"
       await this.selectActiveCustomer()
+      await this.getCustomers()
+      this.getCustomersDebounced = _.debounce(this.getCustomers, 2000, false)
     }
   })
 }
