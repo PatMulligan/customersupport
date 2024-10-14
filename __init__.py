48fe9dff40b71b9755f8afcb2a5849bc130126e1
Patrick Mulligan <patjmulligan@gmail.com>
2024-10-14 13:47:32 +0200

    remove wait_for_paid_invoices from scheduler

--- __init__.py
+++ __init__.py
@@ -57,7 +57,8 @@ def customersupport_start():
         await asyncio.sleep(15)
         await wait_for_nostr_events(nostr_client)
 
-    task1 = create_permanent_unique_task("ext_customersupport_paid_invoices", wait_for_paid_invoices)
+    # task1 = create_permanent_unique_task("ext_customersupport_paid_invoices", wait_for_paid_invoices)
     task2 = create_permanent_unique_task("ext_customersupport_subscribe_to_nostr_client", _subscribe_to_nostr_client)
     task3 = create_permanent_unique_task("ext_customersupport_wait_for_events", _wait_for_nostr_events)
-    scheduled_tasks.extend([task1, task2, task3])
+    # scheduled_tasks.extend([task1, task2, task3])
+    scheduled_tasks.extend([task2, task3])
